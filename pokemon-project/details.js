2c8b2951ac5cf92a80aaea54f79b94b70a032648
VjeraTurk <vjera.turk@gmail.com>
2020-05-02 22:24:50 +0200

    exercise-3 pokemon-project

--- pokemon-project/details.js
+++ pokemon-project/details.js
@@ -1,44 +1,65 @@
-const urlParams = new URLSearchParams(window.location.search);
-const chosenPokemon = pokemon[Number(urlParams.get('pokemon'))];
+// get urlParams from url
+//https://www.sitepoint.com/get-url-parameters-with-javascript/
 
-const wrapperElement = document.querySelector('#app > .details-wrapper')
-
-const numberElement = document.createElement('div');
-numberElement.classList.add('number');
-numberElement.textContent = `#${chosenPokemon.id}`;
-
-const pokemonImageElement = document.createElement('img');
-pokemonImageElement.src = `./images/${chosenPokemon.id.toString().padStart(3, 0)}.png`
-pokemonImageElement.id = chosenPokemon.name.english;
-
-const nameElement = document.createElement('div');
-nameElement.classList.add('name');
-nameElement.textContent = chosenPokemon.name.english;
-
-const typesElement = document.createElement('div');
-typesElement.classList.add('types');
-
-chosenPokemon.type.forEach(p => {
-  const typeElement = document.createElement('span');
-  typeElement.style.background = colors[p];
-  typeElement.textContent = p;
-  typesElement.appendChild(typeElement);
-});
-
-const createStats = (value, key) => {
-  const statsElement = document.createElement('div');
-  statsElement.classList.add('stats');
-  statsElement.textContent = `${key}: ${value}`;
-  return statsElement;
+function getPokemon(){
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const pokemonId = urlParams.get('pokemon');
+  return pokemon[Number(pokemonId)];
 }
 
-wrapperElement.appendChild(numberElement);
-wrapperElement.appendChild(pokemonImageElement);
-wrapperElement.appendChild(nameElement);
-wrapperElement.appendChild(typesElement);
-wrapperElement.appendChild(createStats(chosenPokemon.base.HP, 'HP'));
-wrapperElement.appendChild(createStats(chosenPokemon.base.Attack, 'Attack'));
-wrapperElement.appendChild(createStats(chosenPokemon.base.Defense, 'Defense'));
-wrapperElement.appendChild(createStats(chosenPokemon.base['Sp. Attack'], 'Sp. Attack'));
-wrapperElement.appendChild(createStats(chosenPokemon.base['Sp. Defense'], 'Sp. Defense'));
-wrapperElement.appendChild(createStats(chosenPokemon.base.Speed, 'Speed'));
+// get our chosen pokemon
+const p = getPokemon();
+
+// create number element wih class number
+const pokemonNumberElement = document.createElement('div');
+pokemonNumberElement.className = 'number';
+pokemonNumberElement.textContent = `#${p.id}`;
+
+// create pokemon image
+const pokemonImageElement = document.createElement('img');
+pokemonImageElement.src = `./images/${p.id.toString().padStart(3, '0')}.png`;
+pokemonImageElement.alt = p.name.english;
+pokemonImageElement.id = p.name.english;
+
+// create pokemon name with class name
+const pokemonNameElement = document.createElement('div');
+pokemonNameElement.className = 'name';
+pokemonNameElement.textContent = p.name.english;
+// create pokemon types with class types example: <div><span>Grass</span><span>Poison</span></div>
+const divElement = document.createElement('div');
+divElement.className = 'types';
+
+for(i in p.type){
+  const typeElement = document.createElement('span');
+  typeElement.textContent = p.type[i];
+  typeElement.style.background = `${colors[p.type[i]]}`;
+  divElement.appendChild(typeElement);
+}
+
+let detailsWrapperElement = document.querySelector('.details-wrapper');
+
+detailsWrapperElement.appendChild(pokemonNumberElement);
+detailsWrapperElement.appendChild(pokemonImageElement);
+detailsWrapperElement.appendChild(pokemonNameElement);
+detailsWrapperElement.appendChild(divElement);
+
+for( key in p.base){
+  let statElement = document.createElement('div');
+  statElement.textContent = `${key}: ${p.base[key]}`;
+  statElement.className = 'stats';
+  detailsWrapperElement.appendChild(statElement);
+}
+
+const appWrapperElement = document.createElement('div');
+appWrapperElement.id = 'app';
+
+//document.body.appendChild(appWrapperElement);
+//detailsWrapperElement = appWrapperElement.insertBefore(detailsWrapperElement,null);
+//appWrapperElement.appendChild(detailsWrapperElement);
+//document.body.appendChild(appWrapperElement);
+//appWrapperElement.appendChild(detailsWrapperElement);
+
+// app div se pozicionira sam ?!
+
+//https://lockdown-learning-ix5bxftef.now.sh/pokemon-project/details.html?pokemon=0
