1f15ec5615334612c37cc0c96feb8746e57af136
VjeraTurk <vjera.turk@gmail.com>
2020-05-07 13:20:59 +0200

    my comments TODO: exercse 3 pokemon-project-advanced

--- pokemon-project-advanced/fetch.js
+++ pokemon-project-advanced/fetch.js
@@ -1,22 +1,30 @@
 const url = 'http://localhost:3000/pokemon';
-
+ 
 export async function createPokemon(data = {}) {
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: {
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return await response.json(); // parses JSON response into native JavaScript objects
+  }
+  //takes some data of pokemon, stringifies it and sends to back-end via POST method
+
+export async function deletePokemon(id){
+  //if ID is undifiend show error
+  if(!id){
+    throw new Error('You should pass id');
+  } 
+ 
+  const response = await fetch(`${url}/${id}`, {
+    method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
+    /*headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(data) 
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+    */
   });
-  return await response.json();
-}
-
-export async function deletePokemon(id) {
-  if (!id) {
-    throw new Error('YOU SHOULD PASS ID');
-  } 
-  const response = await fetch(`${url}/${id}`, {
-    method: 'DELETE',
-  });
-  return await response.json();
+  return await response.json(); // parses JSON response into native JavaScript objects
 }
