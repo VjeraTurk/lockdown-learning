c087f4837df0e299b4027fdc2d6c1843373bb090
VjeraTurk <vjera.turk@gmail.com>
2020-05-19 22:01:09 +0200

    completed exercise tasks 1 and 2

--- pokemon-project-advanced/script.js
+++ pokemon-project-advanced/script.js
@@ -27,7 +27,6 @@ const initSearchBox = () => {
   searchTextElement.onkeyup = (event) => filterPokemonList(event.target.value);
 }
 
-
 const createPokemonList = async () => {
   const response = await fetch('http://localhost:3000/pokemon');
 
@@ -61,90 +60,103 @@ const createPokemonList = async () => {
   });
 };
 
+const initSelectTypeElement = (types) =>{
+  const pokemonSelectTypeElement = document.createElement('select');
 
-const initNewPokemonForm = ()=> {
-
-  //const types = colors
-  const newPokemonForm = document.querySelector('.form-wrapper');
-
-  const pokemonEnglishNameElement = document.createElement('input');
-  const pokemonJapaneseNameElement = document.createElement('input');
-  const pokemonChineseNameElement = document.createElement('input');
-  const pokemonFrenchNameElement = document.createElement('input');
-
-  pokemonEnglishNameElement.type = 'text';
-  pokemonEnglishNameElement.placeholder = 'English name';
-
-  pokemonJapaneseNameElement.type = 'text';
-  pokemonJapaneseNameElement.placeholder = 'Japanese name';
-
-  pokemonChineseNameElement.type = 'text';
-  pokemonChineseNameElement.placeholder = 'Chinese name';
-
-  pokemonFrenchNameElement.type = 'text';
-  pokemonFrenchNameElement.placeholder = 'French name';
-
-  newPokemonForm.appendChild(pokemonEnglishNameElement);
-  newPokemonForm.appendChild(pokemonJapaneseNameElement);
-  newPokemonForm.appendChild(pokemonChineseNameElement);
-  newPokemonForm.appendChild(pokemonFrenchNameElement);
-
-  console.log(types.types);
-
-  const pokemon1stTypeElement = document.createElement('select');
-  const pokemon2ndTypeElement = document.createElement('select');
-
-  const defaultOption = document.createElement('option');
-  defaultOption.setAttribute("value", "");
-  defaultOption.textContent = "--";
-
-  pokemon1stTypeElement.appendChild(defaultOption);
-  pokemon2ndTypeElement.appendChild(defaultOption);
-
-  types.types.forEach((type) => {
+  types.forEach((type) => {
     const option = document.createElement('option');
-    option.setAttribute("name",type);
-    option.setAttribute("id",type);
-    option.setAttribute("value",type);
-    option.setAttribute("label",type);
+    option.name = type;
+    option.value= type;
+    option.setAttribute('class',type);
     option.textContent= type;
-
-    pokemon1stTypeElement.appendChild(option);
-    pokemon2ndTypeElement.appendChild(option); // deappenda iz 1stTypeElement
+    pokemonSelectTypeElement.appendChild(option);
   })
-  newPokemonForm.appendChild(pokemon1stTypeElement);
-  newPokemonForm.appendChild(pokemon2ndTypeElement);
+  return pokemonSelectTypeElement;
+}
+
+function hideOption(event) {
+  const thisPokemonSelectTypeElement = event.target;
+  const otherPokemonSelectTypeElement =  event.target.id === document.querySelector(`#type-1`).id ? document.querySelector(`#type-2`): document.querySelector(`#type-1`);
+
+  const options = otherPokemonSelectTypeElement.children;
+    for (let i = 0; i < options.length; i++) {
+      options[i].hidden = false;
+    }
+
+    try{
+    otherPokemonSelectTypeElement.querySelector(`.${thisPokemonSelectTypeElement.value}`).hidden = true;
+  }catch(exception){
+    console.log('no default option');
+  }
+}
+
+const initTextElements = (data) => {
+
+  let textElements = new Array();
+
+  data.forEach((item) => {
+    const baseElement = document.createElement('input');
+    baseElement.type = 'text';
+    baseElement.placeholder = item;
+    //console.log(item);
+    textElements.push(baseElement);
+  })
+  return textElements;
+}
+
+const initCreatePokemonForm = () => {
+
+  const createPokemonForm = document.querySelector('#create-pokemon-form');
+  const nameElements = initTextElements (['English name','Japanese name', 'Chinese name', 'French name']);
+  nameElements.forEach(nameElement => createPokemonForm.append(nameElement));
+  //const types = colors.<var names>
+
+  const pokemon1stTypeElement = initSelectTypeElement(types.types);
+  const pokemon2ndTypeElement = initSelectTypeElement(types.types);
+
+  let defaultOption = document.createElement('option');
+  defaultOption.textContent = "--";
+  defaultOption = pokemon2ndTypeElement.insertBefore(defaultOption, pokemon2ndTypeElement.firstChild);
+  pokemon2ndTypeElement.value = defaultOption.textContent;
+
+  pokemon1stTypeElement.id = "type-1";
+  pokemon2ndTypeElement.id = "type-2";
+
+  //disable choosing the same type twice:
+  pokemon1stTypeElement.onchange = hideOption;
+  pokemon2ndTypeElement.onchange = hideOption;
+
+  createPokemonForm.appendChild(pokemon1stTypeElement);
+  createPokemonForm.appendChild(pokemon2ndTypeElement);
+
+  const baseElements = initTextElements(['HP','Attack', 'Defense', 'Sp. Attack','Sp. Defense', 'Speed']);
+  baseElements.forEach(baseElement => createPokemonForm.append(baseElement));
 
   const submitButtonElement = document.createElement('button');
 
   submitButtonElement.onclick = ()=>{
     console.log('Submit');
-    //const type = new Array;
-    //if (pokemon1stTypeElement)
-
-    //new Pokemon();
     createPokemon({
       name: {
-        english: pokemonEnglishNameElement.value,
-        japanese: pokemonJapaneseNameElement.value,
-        chinese: pokemonChineseNameElement.value,
-        french: pokemonFrenchNameElement.value
+        english: nameElements[0].value,
+        japanese: nameElements[1].value,
+        chinese: nameElements[2].value,
+        french: nameElements[3].value
       },
-      type: ["Grass", // ? length 
-      "Poison"
-    ],
+        type : pokemon2ndTypeElement.value == '--' ? [pokemon1stTypeElement.value] : [pokemon1stTypeElement.value, pokemon2ndTypeElement.value]      ,
       base: {
-        HP: 45,
-        Attack: 49,
-        Defense: 49,
-        "Sp. Attack": 65,
-        "Sp. Defense": 65,
-        Speed: 45
+        HP: Number(baseElements[0].value),
+        Attack: Number(baseElements[1].value),
+        Defense: Number(baseElements[2].value),
+        "Sp. Attack": Number(baseElements[3].value),
+        "Sp. Defense": Number(baseElements[4].value),
+        Speed: Number(baseElements[5].value)
       }
     });
+    console.log("created");
   }
   submitButtonElement.textContent = "Done";
-  newPokemonForm.appendChild(submitButtonElement);
+  createPokemonForm.appendChild(submitButtonElement);
 
 }
 
@@ -156,12 +168,17 @@ const updatePokemonList =  ()=> {
 const init = () => {
   createPokemonList();
   initSearchBox();
-  initNewPokemonForm();
+  initCreatePokemonForm();
 };
 
 init();
 
 // 1. Create a form for new pokemon creation
+/*
+DONE
+additional TODOs:
+  - check for empty inputs
+*/
 
 /*createPokemon({
   name: {
@@ -184,8 +201,17 @@ init();
 });*/
 
 // 2. Delete pokemon on click (including newly created pokemon!)
+/*
+DONE
+*/
 
 //  deletePokemon(5);
 
 // 3. On every delition or creation of a pokemon, you should update (refetch) the data
 //function updatePokemonList that will **do some magic**
+
+/*
+TODO:
+  - updatePokemonList nakon delete
+  - izgleda da se nakon create stranica sama refresha
+*/
